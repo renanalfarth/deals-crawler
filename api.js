f35ce6720bba1adea235eba74e9665d9f46d1c57
Renan Alfarth <renan.alfarth@capitanigroup.com.br>
2017-07-26 18:03:07 -0300

    Initial commit

---
+++ api.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+//global libs
+global.config  = {};
+global.request = require('request');
+global.cheerio = require('cheerio');
+
+//local libs
+var	bodyParser 			 = require('body-parser'),
+		util 						 = require('util'),
+		express 				 = require('express'),
+		cors 						 = require('cors'),
+		app 						 = express();
+
+//--- app routes
+global.craw  = require('./functions/craw');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.raw({ limit: '50mb' }));
+
+app.options('*', cors());
+
+app.get('/promocoes', craw.gocraw);
+
+//craw.gocraw();
+
+app.listen(8888, function(){
+	console.log('#############################');
+	console.log('||||||||# CRAWLER #||||||||');
+	console.log('#############################');
+});
---
+++ functions/craw.js
@@ -0,0 +1,42 @@
+exports.gocraw = function (exp_req, exp_res) {
+
+  // HARDMOB
+  request({
+    url: 'http://www.hardmob.com.br/promocoes',
+    method: 'GET',
+    strictSSL: false,
+    encoding: 'ascii',
+    headers: {
+      'User-Agent': 'Midori',
+    },
+    //form:{'email-login':'renan.fire12@gmail.com'}
+    }, function(err, res) {
+
+        var result = res.body;
+
+        var $ = cheerio.load(result);
+
+        $('#threads').filter(function() {
+          var data = $(this);
+
+          var promotions = [];
+          promotions.push({pagina : 'Hardmob'});
+
+          $(data.children()).each(function(i, elem) {
+
+            promotions.push({
+              titulo : $(this).children().children().find('.title').text(),
+              link   : $(this).children().children().find('.title').attr('href')
+            });
+
+          });
+
+          exp_res.json(promotions);
+
+        });
+
+
+
+    });
+
+};
